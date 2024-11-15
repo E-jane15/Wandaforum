5cfa971c6c25cff71ed00f66e26e2a4c2cbd11df
kelagb45 <agbornkongho356@gmail.com>
2024-11-15 14:09:52 +0100

    added assets

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,18 +3,25 @@ import logo from '../../assets/wandaforum_icon.svg'
 
 const Navbar = () => {
   return (
-    <div className='text-white w-full'>
-        <div className=' flex items-center justify-center md:justify-between px-20 my-6 sticky top-0'>
+    <div className="text-white w-full">
+      <div className=" flex items-center justify-center md:justify-between px-20 my-6 sticky top-0">
         <img src={logo} alt="" />
-        <a href="" className='text-lg font-medium hover:text-orange'>Home</a>
-        <a href="" className='text-lg font-medium hover:text-orange'>Community</a>
-        <a href="" className='text-lg text-orange font-medium'>Peermocks</a>
-        <a href="" className='text-lg font-medium hover:text-orange'>Pricing</a>
-        <button className='bg-purple py-3 px-7 rounded-full'>Signup</button>
-        </div>
-        
+        <a href="" className="text-lg font-medium text-orange ">
+          Home
+        </a>
+        <a href="" className="text-lg font-medium hover:text-orange">
+          Community
+        </a>
+        <a href="" className="text-lg  hover:text-orange font-medium">
+          Peermocks
+        </a>
+        <a href="" className="text-lg font-medium hover:text-orange">
+          Pricing
+        </a>
+        <button className="bg-purple py-3 px-7 rounded-full">Signup</button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Navbar
