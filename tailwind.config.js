c6d4ccb581fc804b6e3575c9c28f3736bf49b7b4
kelagb45 <agbornkongho356@gmail.com>
2024-11-20 09:53:37 +0100

    Edited the App.jsx and tailwind.config.js files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors:{
-        purple: '#7A24A1',
-        darkpurple: '#281b32',
-        orange: '#FEAA53',
-        purple2: '#C749E4',
-        purple3: '#332C3F',
-        gold: '#DFAf37'
+      colors: {
+        purple: "#7A24A1",
+        darkpurple: "#281b32",
+        orange: "#FEAA53",
+        purple2: "#C749E4",
+        purple3: "#332C3F",
+        gold: "#DFAf37",
+        purple4: "#261738",
+        purple5: "#41264E",
       },
-      fontFamily:{
-        sans:['Montserrat', 'sans-serif'],
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
+      },
+      animation: {
+        float: "float 5s ease-in-out infinite",
+      },
+      keyframes: {
+        float: {
+          "0%, 100%": { transform: "translateY(0px)" },
+          "50%": { transform: "translateY(-20px)" },
+        },
       },
     },
   },
